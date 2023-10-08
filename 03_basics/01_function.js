d5d87e3bef3af95ee6f00749967b753e15e24c40
Nishant Bhardwaj <126180802+Nishant1333@users.noreply.github.com>
2023-10-08 15:59:56 +0000

    .devcontainer

---
+++ 03_basics/01_function.js
@@ -0,0 +1,43 @@
+// function myName(){
+//     console.log("n");
+//     console.log("i");
+//     console.log("s");
+//     console.log("h");
+//     console.log("u");
+
+// }
+// myName()
+// function addTwoNum(num1,num2){
+//     console.log(num1+num2)
+// }
+// addTwoNum(4,6)
+// function loginUser(username = "sam"){
+//     if(!username){
+//         console.log("Enter user name");
+//         return
+        
+//     }
+//     return `hello ${username} just logged in`
+// }
+// console.log(loginUser("pillu"))
+
+// function calculatePrice(...num1){
+//     return num1
+// }
+// console.log(calculatePrice(244,334,55));
+
+
+// const user ={
+//     username:"nishu",
+//     price:223
+// }
+// function handleObj(anyobject){
+//     console.log(`username is ${anyobject.username} and price is${anyobject.price} `)
+// }
+// handleObj(user)
+
+const newarr=[200,33,456,6778,]
+function returnArr(getArray){
+    return getArray[3]
+}
+console.log(returnArr(newarr));
